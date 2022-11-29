7f18445afdbfef50e5a048240de405b1bbd93031
cw <chimingwang0@gmail.com>
2022-11-28 22:29:00 -0500

    Create lesson15cw_forloop.java

---
+++ Lesson15Classwork/lesson15cw_forloop.java
@@ -0,0 +1,42 @@
+/***
+Chiming Wang
+10/18/2022
+Java Period 8
+Lesson 15
+***/
+
+//imports packages
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.Math;
+
+public class lesson15cw_forloop
+{
+   
+   public static void main (String[] args)
+   {
+     
+            
+      //main part of program
+      for (int i = 45; i >= -45; i--) {
+         
+         
+         double x = i%9;
+         
+         if (x == 0) {
+         System.out.println(i + ", ");
+         }
+         
+
+
+      }
+      
+      
+      
+
+      
+      
+            
+      
+   }
+}
