e3a2787896848f7f7dfaede37d09b61b6c0da13d
cw <chimingwang0@gmail.com>
2022-11-28 21:49:04 -0500

    Create lesson6cw.java

---
+++ Lesson6Classwork/lesson6cw.java
@@ -0,0 +1,10 @@
+public class lesson6cw 
+{
+   
+   public static void main (String[] args)
+   {
+      System.out.println("Java is fun");
+
+      System.out.println("So this guy in the front of the room says...");
+   }
+}
