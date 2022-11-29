ca3096ab08207d3870cae84cd243ae01b84df547
cw <chimingwang0@gmail.com>
2022-11-28 21:52:14 -0500

    Create lesson7cw.java

---
+++ Lesson7Classwork/lesson7cw.java
@@ -0,0 +1,29 @@
+// hi
+public class lesson7cw 
+{
+   
+   public static void main (String[] args)
+   {
+      
+      //declares variables
+      int int1 = 69;
+      char char1 = 'K';
+      boolean bool1 = true;
+      double a, b, c; 
+      a = 1.2;
+      b = 2.1232131;
+      c = 3.69;
+      
+      System.out.println(int1); //prints int1
+
+      System.out.println(char1); //prints char
+      
+      System.out.println(bool1); //prints bool1
+      
+      //prints doubles
+      System.out.println(a); 
+      System.out.println(b);
+      System.out.println(c);
+      
+   }
+}
