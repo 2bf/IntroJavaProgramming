9409e52c70863f64fb7b0767bf755a1273d49e65
cw <chimingwang0@gmail.com>
2022-12-06 13:28:56 -0500

    Update Palindrome.java

--- Project1/Palindrome.java
+++ Project1/Palindrome.java
@@ -12,28 +12,29 @@ import javax.swing.*;
 public class Palindrome{
 
 	public static void main(String[] args){
-      String input, result, palindrome;
+      String input, result, palindrome; //declare variables in which values will be stored in
       int again;
 
       do {
          input = JOptionPane.showInputDialog ("Enter your string:");
-         result = "That string reverse is: ''" + (reverseString(input) + "''");
-         JOptionPane.showMessageDialog (null, result);
-         if (isPalindrome(input) == true) {
+         result = "That string reverse is: ''" + (reverseString(input) + "''"); //calls reverseString method and puts everything into a string
+         JOptionPane.showMessageDialog (null, result); //window that shows the result and displays value
+         if (isPalindrome(input) == true) { //tells user if the string is a palindrome or not
             palindrome = ("The string ''" + input + "'' is a palindrome.");
          } else {
             palindrome = ("The string ''" + input + "'' is not a palindrome.");
          }
-         JOptionPane.showMessageDialog (null, palindrome);
+         JOptionPane.showMessageDialog (null, palindrome); 
          again = JOptionPane.showConfirmDialog (null, "Would you like to check another string?");
       } // do end
       while (again == JOptionPane.YES_OPTION);
 	} //public static void main end
    
+  
    public static String reverseString(String str){
       String reverse = "";
       char scan;
-      for (int i=0; i<str.length(); i++){
+      for (int i=0; i<str.length(); i++){ //takes each character in input string and reverses it by putting each character in front of the character that was previously in front of it   
          scan = str.charAt(i);
          reverse = scan + reverse;
       } //for loop end
