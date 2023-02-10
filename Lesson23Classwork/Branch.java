2080dbb77f8ab2025e9dec461102bb021550021d
cw <chimingwang0@gmail.com>
2023-02-09 19:28:05 -0500

    Create Branch.java

---
+++ Lesson23Classwork/Branch.java
@@ -0,0 +1,57 @@
+/***
+Chiming Wang
+01/23/2022
+Java Period 8
+Lesson 23 Classwork
+***/
+
+//imports packages
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.Math;
+
+public class Branch {
+
+   //declaring instance variables
+   private String brAddress, accType;
+   private int phoneNum, customerID, customerBal;
+   private BankTeller bankteller;
+   
+   //constructor (??)
+   public Branch() {
+    brAddress = "";
+    phoneNum = 0;
+    
+   }
+   
+
+   public void setBrAddress (String name) {
+      brAddress = name;
+   }
+   public void setPhoneNum (int num) {
+      phoneNum = num;
+   }
+
+   
+
+   public String getBrAddress() {
+      return brAddress;
+   }
+   public int getPhoneNum() {
+      return phoneNum;
+   }
+  
+   
+   
+   public String toString() {
+      String result = "";
+    
+      result = "Address: " + brAddress + "\n";
+      result += "Phone Numbers: " + phoneNum + "\n";
+
+
+    
+      return result;
+   }
+   
+} //public class end
