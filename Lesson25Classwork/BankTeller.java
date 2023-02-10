de1134686d9103df112920392f00411ca4b0d6dd
cw <chimingwang0@gmail.com>
2023-02-09 19:32:16 -0500

    Create BankTeller.java

---
+++ Lesson25Classwork/BankTeller.java
@@ -0,0 +1,75 @@
+/***
+Chiming Wang
+01/23/2022
+Java Period 8
+Lesson 23 Classwork
+***/
+
+//imports packages
+import javax.swing.*;
+
+public class BankTeller {
+
+   //declaring instance variables
+   private String tellerName;
+   private int tellerID, bal, depBal, withBal;
+   private static int count = 0;
+   
+   //constructor (??)
+   public BankTeller() {
+    tellerName = "";
+    tellerID = 0;
+    bal = 0;
+    
+   }
+   
+
+   public void setTellerName (String name) {
+      tellerName = name;
+      count++;
+   }
+   public void setTellerID (int id) {
+      tellerID = id;
+   }
+   public void setAccountBalance (int balance){
+      this.bal = bal;
+   }
+   
+
+   public String getTellerName() {
+      return tellerName;
+   }
+   public int getTellerID() {
+      return tellerID;
+   }
+   public int getAccountBalance (){
+      return bal;
+   }
+   
+   public void setDeposit(int deposit){
+      bal = bal + deposit;
+   }
+   public void setWithdraw(int withdraw){
+      bal = bal - withdraw;
+   }
+   
+   public int getDeposit() {
+      return bal;
+   }
+   public int getWithdraw() {
+      return bal;
+   }
+   
+   public static int getCount (){
+     return count;
+   }
+   
+   public String toString() {
+      String result = "";
+      result = "Teller: " + tellerName + "\n";
+      result += "Teller ID: " + tellerID + "\n";
+      result += "Number of Tellers: " + count + "\n";
+      return result;
+   }
+   
+} //public class end
