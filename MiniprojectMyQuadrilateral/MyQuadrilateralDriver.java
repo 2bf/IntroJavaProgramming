514f797e0e07c58c20cf3bba862a416db12a7883
cw <chimingwang0@gmail.com>
2023-03-11 00:04:49 -0500

    Update MyQuadrilateralDriver.java

--- MiniprojectMyQuadrilateral/MyQuadrilateralDriver.java
+++ MiniprojectMyQuadrilateral/MyQuadrilateralDriver.java
@@ -13,27 +13,29 @@ public class MyQuadrilateralDriver {
       MyPoint p2 = new MyPoint(4, 7);
       MyPoint p3 = new MyPoint(2, 8);
       MyPoint p4 = new MyPoint(-1,5);
-      MyQuadrilateral quad = new MyQuadrilateral(p1, p2, p3, p4, "ABCD");
+      String name = "ABCD";
+      MyQuadrilateral quad = new MyQuadrilateral(p1, p2, p3, p4, name);
       
       MyPoint o1 = new MyPoint(-2, 1);
       MyPoint o2 = new MyPoint(3, 2);
       MyPoint o3 = new MyPoint(1, 6);
       MyPoint o4 = new MyPoint(-3, 7);
-      MyQuadrilateral other = new MyQuadrilateral(o1, o2, o3, o4, "EFGH");
+      String othername = "EFGH";
+      MyQuadrilateral other = new MyQuadrilateral(o1, o2, o3, o4, othername);
 
       System.out.println(quad);
       System.out.println(other);
       
       String result = "";
-      result += "Perimeter (" + quad.getName() + "): " + quad.findPerimeter() + "\n";
-      result += "Perimeter (" + other.getName() + "): " + other.findPerimeter() + "\n";
-      result += "Area (" + quad.getName() + "): " + quad.findArea() + "\n";      
-      result += "Area (" + other.getName() + "): " + other.findArea() + "\n";
-      result += "Diagonal difference (" + quad.getName() + "): " + quad.diagonalDifference() + "\n";
-      result += "Diagonal difference (" + other.getName() + "): " + other.diagonalDifference() + "\n";
-      result += "isIsoscelesTrapezoid (" + quad.getName() + "): " + quad.isIsoscelesTrapezoid() + "\n";
-      result += "isIsoscelesTrapezoid (" + other.getName() + "): " + other.isIsoscelesTrapezoid() + "\n";
-      result += "isBigger (" + quad.getName() + " is bigger than " + other.getName() + "): " + quad.isBigger(other) + "\n";
+      result += "Perimeter (" + name + "): " + quad.findPerimeter() + "\n";
+      result += "Perimeter (" + othername + "): " + other.findPerimeter() + "\n";
+      result += "Area (" + name + "): " + quad.findArea() + "\n";      
+      result += "Area (" + othername + "): " + other.findArea() + "\n";
+      result += "Diagonal difference (" + name + "): " + quad.diagonalDifference() + "\n";
+      result += "Diagonal difference (" + othername + "): " + other.diagonalDifference() + "\n";
+      result += "isIsoscelesTrapezoid (" + name + "): " + quad.isIsoscelesTrapezoid() + "\n";
+      result += "isIsoscelesTrapezoid (" + othername + "): " + other.isIsoscelesTrapezoid() + "\n";
+      result += "isBigger (" + name + " is bigger than " + othername + "): " + quad.isBigger(other) + "\n";
       System.out.println(result);
 
 
