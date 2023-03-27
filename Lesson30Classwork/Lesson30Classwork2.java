6dd8ac5d1fd9b965469253dfdc8f56b317d821d9
chiming <chimingwang0@gmail.com>
2023-03-27 13:49:41 -0400

    Create Lesson30Classwork2.java

---
+++ Lesson30Classwork/Lesson30Classwork2.java
@@ -0,0 +1,31 @@
+/***
+Chiming Wang
+03/27/2023
+Java Period 8
+Lesson 30 Classwork 
+***/
+
+//imports packages
+import javax.swing.*;
+
+public class Lesson30Classwork2 {
+   
+   public static void main (String[] args) 
+   { 
+      int[][] table = new int[10][10]; 
+
+      // Load the table with values 
+      for (int row = 1; row < table.length; row++) 
+         for (int col=1; col < table[row].length; col++) 
+            table[row][col] = row * col; 
+
+      // Print the table 
+      for (int row=1; row < table.length; row++) 
+      { 
+         for (int col=1; col < row+1; col++) 
+            System.out.print (table[row][col] + "\t"); 
+         System.out.println(); 
+      } 
+   } 
+      
+} //public class end
